48cb3b53ac88353621517ab238d09056365b053a
K4JIC <Tomoya0815zak@gmail.com>
2026-04-16 20:16:33 +0900

    toriaezu

--- makelink.sh
+++ makelink.sh
@@ -7,9 +7,13 @@ echo "stow を使用してシンボリックリンクを更新します"
 cd "$DOTFILES_DIR"
 
 # 管理対象のパッケージをリストアップ（.git などを除くディレクトリ）
-PACKAGES=(bash zsh git vim zed waybar ghostty niri wezterm lazygit kanshi)
+PACKAGES=(*/)
+PACKAGES=("${PACKAGES[@]%/}") # 末尾のスラッシュを削除
 
 for pkg in "${PACKAGES[@]}"; do
+    # .git などの隠しディレクトリを除外
+    [[ "$pkg" == .* ]] && continue
+    
     if [ -d "$pkg" ]; then
         echo "$pkg のリンクを作成/更新します"
         stow -R "$pkg"
